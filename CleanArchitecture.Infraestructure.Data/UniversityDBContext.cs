b01104bc9493455865e2df58b72dce9b8ad2dc98
George Studenko <gnstudenko@gmail.com>
2019-08-26 10:35:28 +0200

    Implement UniversityDBContext

--- CleanArchitecture.Infraestructure.Data/UniversityDBContext.cs
+++ CleanArchitecture.Infraestructure.Data/UniversityDBContext.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CleanArchitecture.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.Infraestructure.Data
 {
-    public class UniversityDBContext
+    public class UniversityDBContext : DbContext
     {
+        public UniversityDBContext (DbContextOptions options) : base(options)
+        {
 
-    }
+        }
 
-
+        public DbSet<Course> Courses { get; set; }
+    }    
 }
