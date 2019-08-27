0f6d3828f9c76dd0da8790c804af2fb59d637213
George Studenko <gnstudenko@gmail.com>
2019-08-27 22:45:27 +0200

    Create DependencyContainer

---
+++ CleanArchitecture.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+
+        }
+
+    }
+}
