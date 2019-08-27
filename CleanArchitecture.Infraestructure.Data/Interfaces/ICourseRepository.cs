35c6d4f7be57f6d874742b8644fcc72842dacfc4
George Studenko <gnstudenko@gmail.com>
2019-08-27 22:50:10 +0200

    Add Repository

---
+++ CleanArchitecture.Infraestructure.Data/Interfaces/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Infraestructure.Data.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
---
+++ CleanArchitecture.Infraestructure.Data/Repository/CourseRepository.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Models;
+using CleanArchitecture.Infraestructure.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Infraestructure.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        public IEnumerable<Course> GetCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
